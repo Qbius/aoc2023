26aeba4843d825ce29aa81bab250722160761a76
Michał Surówka <mich@lsurowka.email>
2023-11-19 18:52:33 +0100

    init

---
+++ src/aoc/d14.rs
@@ -0,0 +1,15 @@
+use crate::common::Res;
+pub type Input = String;
+pub type Output = i32;
+
+pub fn parse(input: String) -> Input {
+    input
+}
+
+pub fn first(input: Input) -> Res<Output> {
+    Err("Not implemented")
+}
+
+pub fn second(input: Input) -> Res<Output> {
+    Err("Not implemented")
+}
