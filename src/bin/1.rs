eb548f1d0a75c17e95494aa9a83a4b1f91a63936
Michał Surówka <mich@lsurowka.email>
2023-12-02 19:11:13 +0100

    questions are as important as answers

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -13,21 +13,17 @@ xtwone3four
 zoneight234
 7pqrstsixteen";
 
-fn concatenate_numbers(a: Option<u32>, b: Option<u32>) -> Option<u32> {
-    a.and_then(|a| b.map(|b| a.to_string() + &b.to_string())).and_then(|s| s.parse::<u32>().ok())
-}
-
 fn first(input: &str) -> u32 {
     let parsed: Vec<Vec<u32>> = input.split('\n').map(|line| line.chars().filter_map(|c| c.to_digit(10)).collect()).collect();
-    parsed.into_iter().filter_map(|v| concatenate_numbers(v.first().copied(), v.last().copied())).sum()
+    parsed.into_iter().filter_map(|v| Some(v.first()? * 10 + v.last()?)).sum()
 }
 
-fn second(input: &str) -> u32 {
+fn second(input: &str) -> usize {
     let substrs2val: Vec<(usize, String)> = SDIGITS.iter().cloned().map(String::from).enumerate().chain((1..=9).map(|i| i.to_string()).enumerate()).map(|(i, substr)| (i + 1, substr)).collect();
     input.split('\n').filter_map(|line| {
-        let frst_opt = substrs2val.iter().filter_map(|(val, substr)| line.find(substr).map(|pos| (pos, val))).min().map(|(_pos, val)| val.clone() as u32);
-        let last_opt = substrs2val.iter().filter_map(|(val, substr)| line.rfind(substr).map(|pos| (pos, val))).max().map(|(_pos, val)| val.clone() as u32);
-        concatenate_numbers(frst_opt, last_opt)
+        let frst_opt = substrs2val.iter().filter_map(|(val, substr)| line.find(substr).map(|pos| (pos, val))).min().map(|(_pos, val)| val);
+        let last_opt = substrs2val.iter().filter_map(|(val, substr)| line.rfind(substr).map(|pos| (pos, val))).max().map(|(_pos, val)| val);
+        Some(frst_opt? * 10 + last_opt?)
     }).sum()
 }
 
