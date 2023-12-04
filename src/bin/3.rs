5ff4584fc5aa530145a3559fcbf5500fb674729b
qbius <theqbius@gmail.com>
2023-12-04 01:32:56 +0100

    day 3

--- src/bin/3.rs
+++ src/bin/3.rs
@@ -1,17 +1,63 @@
+use std::collections::HashMap;
+
 use aoc::*;
+use regex::Regex;
+use std::cmp::max;
 
 const EXAMPLE: &str = "
-hehe: 7
-hehe: 2
-hehe: 0";
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
 
-fn first(input: &str) -> i32 {
-    input.len() as i32
+fn get_info(lines: Vec<String>) -> Option<(Vec<(usize, usize, usize, usize)>, HashMap<(usize, usize), char>)> {
+    let re_numbers = Regex::new(r"\d+").ok()?;
+    let re_symbols = Regex::new(r"[^\.\d]").ok()?;
+    let (all_numbers, all_symbols): (Vec<_>, Vec<_>) = lines.into_iter().enumerate().map(|(y, line)| {
+        let numbers: Vec<_> = re_numbers.find_iter(&line).filter_map(|matched| Some((matched.start(), y, matched.len(), matched.as_str().parse::<usize>().ok()?))).collect();
+        let symbols: Vec<_> = re_symbols.find_iter(&line).filter_map(|matched| Some(((matched.start(), y), matched.as_str().chars().nth(0)?))).collect();
+        (numbers, symbols)
+    }).unzip();
+    let numbers: Vec<_> = all_numbers.into_iter().flat_map(Vec::into_iter).collect();
+    let symbols: HashMap<(usize, usize), char> = all_symbols.into_iter().flat_map(Vec::into_iter).collect();
+    Some((numbers, symbols))
+}
+
+fn adjacent(point: (usize, usize), len: usize) -> Vec<(usize, usize)> {
+    let (x, y) = point;
+    (max(y, 1) - 1..=y + 1).flat_map(move |b| (max(x, 1) - 1..=x + len).map(move |a| (a, b))).collect()
 }
 
 #[lines]
-fn second(lines: Vec<String>) -> i32 {
-    lines.len() as i32
+fn first(lines: Vec<String>) -> usize {
+    let (numbers, symbols) = get_info(lines).expect("Something went wrong with the input");
+    numbers.into_iter().filter_map(|(x, y, len, num)| {
+        match adjacent((x, y), len).iter().any(|point| symbols.contains_key(point)) {
+            true => Some(num),
+            false => None,
+        }
+    }).sum()
+}
+
+#[lines]
+fn second(lines: Vec<String>) -> usize {
+    let (numbers, symbols) = get_info(lines).expect("Something went wrong with the input");
+    symbols.into_iter().filter(|&(_, symbol)| symbol == '*').filter_map(|(point, _)| {
+        let adj = adjacent(point, 1);
+        let adj_numbers: Vec<_> = numbers.iter().filter(|(x, y, len, _)| (x.clone()..x.clone() + len.clone()).map(|a| (a, y.clone())).any(|p| adj.contains(&p))).map(|(_, _, _, num)| num.clone()).collect();
+        if adj_numbers.len() == 2 {
+            Some(adj_numbers.first()? * adj_numbers.last()?)
+        }
+        else {
+            None
+        }
+    }).sum()
 }
 
 aoc!();
--- src/lib/aoc_maker/src/lib.rs
+++ src/lib/aoc_maker/src/lib.rs
@@ -18,8 +18,32 @@ pub fn lines(_: TokenStream, input: TokenStream) -> TokenStream {
 
     let output = quote! {
         #visibility fn #fn_name(input: &str) #return_type {
-            let lines: Vec<String> = input.split('\n').map(String::from).collect();
+            let lines: Vec<String> = input.split('\n').map(|line| line.trim().to_string()).collect();
             #details_name(lines)
+        }
+
+        fn #details_name(#inputs) #return_type #block
+    };
+
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn grid(_: TokenStream, input: TokenStream) -> TokenStream {
+    let function = parse_macro_input!(input as ItemFn);
+
+    let fn_name = &function.sig.ident;
+    let details_name = syn::Ident::new(&format!("{}_details", fn_name.to_string()), function.sig.ident.span());
+
+    let visibility = &function.vis;
+    let inputs = &function.sig.inputs;
+    let return_type = &function.sig.output;
+    let block = &function.block;
+
+    let output = quote! {
+        #visibility fn #fn_name(input: &str) #return_type {
+            let grid: std::collections::HashMap<(usize, usize), char> = input.split('\n').enumerate().flat_map(move |(y, line)| line.chars().enumerate().map(move |(x, c)| ((x, y), c))).collect();
+            #details_name(grid)
         }
 
         fn #details_name(#inputs) #return_type #block
