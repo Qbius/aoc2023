70cb5f2d04d795fd9c1f50b04970e246047676ed
qbius <theqbius@gmail.com>
2023-12-04 06:37:58 +0100

    day 4 part 1

---
+++ src/bin/4.rs
@@ -0,0 +1,35 @@
+use aoc::*;
+
+const EXAMPLE: &str = "
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+#[lines]
+fn parse_cards(lines: Vec<String>) -> Vec<(Vec<usize>, Vec<usize>)> {
+    lines.into_iter().filter_map(|line| {
+        let numbers_str = line.split(':').nth(1)?;
+        let split: Vec<_> = numbers_str.split('|').collect();
+        let winning: Vec<_> = split.first()?.split(' ').filter_map(|e| e.parse::<usize>().ok()).collect();
+        let inventory: Vec<_> = split.last()?.split(' ').filter_map(|e| e.parse::<usize>().ok()).collect();
+        Some((winning, inventory))
+    }).collect()
+}
+
+fn first(cards: Vec<(Vec<usize>, Vec<usize>)>) -> usize {
+    cards.into_iter().map(|(winning, inventory)| inventory.into_iter().filter(|num| winning.contains(num)).count()).map(|count| {
+        match count {
+            0 => 0 as usize,
+            n => (2 as usize).pow((n - 1) as u32)
+        }
+    }).sum()
+}
+
+fn second(_cards: Vec<(Vec<usize>, Vec<usize>)>) -> usize {
+    0
+}
+
+aoc!(parse_cards);
