bad666464881b8eff37a984f493358d3cc6d4643
qbius <theqbius@gmail.com>
2023-12-11 03:37:58 +0100

    day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -55,7 +55,7 @@ impl Loop {
 }
 
 impl Iterator for Loop {
-    type Item = (usize, usize);
+    type Item = ((usize, usize), char);
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.current {
@@ -81,7 +81,7 @@ impl Iterator for Loop {
                 self.point = dir.traverse(self.point);
                 self.current = self.gd.get(&self.point).expect("went off the grid").clone();
                 self.prev = dir.mirror();
-                Some(self.point)
+                Some((self.point, self.current))
             }
         }
     }
@@ -94,16 +94,40 @@ fn first(gd: Grid) -> usize {
 
 #[grid]
 fn second(gd: Grid) -> usize {
-    let lp: Vec<(usize, usize)> = Loop::new(gd).collect();
-    println!("{lp:?}");
-    0
+    let mut rows = HashMap::<usize, Vec<(usize, char)>>::new();
+    Loop::new(gd).for_each(|((x, y), c)| rows.entry(y).or_default().push((x, c)));
+    rows.into_iter().filter_map(|(_, xs2cs)| {
+        let as_map: HashMap<_, _> = xs2cs.into_iter().collect();
+        let start = as_map.keys().min()?.clone();
+        let end = as_map.keys().max()?.clone();
+        let (sum, false, _) = (start..=end).fold((0, false, None), |(acc, in_loop, prevchar), i| {
+            let ch = as_map.get(&i);
+            let res = match ch {
+                Some('|') => (acc, !in_loop, prevchar),
+                Some('F') => (acc, in_loop, Some('F')),
+                Some('L') => (acc, in_loop, Some('L')),
+                Some('S') => match prevchar {Some('F') => (acc, !in_loop, None), Some('L') => (acc, in_loop, None), _ => panic!("strangle loop")},
+                Some('J') => match prevchar {Some('F') => (acc, !in_loop, None), Some('L') => (acc, in_loop, None), _ => panic!("strangle loop")},
+                Some('7') => match prevchar {Some('F') => (acc, in_loop, None), Some('L') => (acc, !in_loop, None), _ => panic!("strangle loop")},
+                Some('-') => (acc, in_loop, prevchar),
+                _ => (acc + match in_loop {true => 1, false => 0}, in_loop, prevchar),
+            };
+            res
+        }) else { panic!("strange loop") };
+        Some(sum)
+    }).sum()
 }
 
 const EXAMPLE: &str = "
--L|F7
-7F-7|
-L|7||
--L-S|
-L|-JF";
+FF7F7F7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LSLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
 
 aoc!();
